c941423445ebc929290fc9690d740c02601f28c5
Vaibhav Nirmal <vaibhavnirmal2001@gmail.com>
2022-11-14 21:33:29 +0530

    starting DOM

--- Js/script.js
+++ Js/script.js
@@ -140,15 +140,15 @@
 
 // Object
 
-let person={
-    firstName: 'vaibhav',
-    lastName: 'Nirmal'
-};
+// let person={
+//     firstName: 'vaibhav',
+//     lastName: 'Nirmal'
+// };
 
 // console.log(person.firstName+  " "+ person.lastName);
 
 // console.log(person['firstName']);
-person.age='22';
+// person.age='22';
 
 // console.log(person);
 
@@ -162,12 +162,134 @@ person.age='22';
 
 // for in lopp
 
-for (let i in person) {
-    console.log( i+" : "+ person[i]);
-}
+// for (let i in person) {
+//     console.log( i+" : "+ person[i]);
+// }
 
 
- 
+
+
+
+
+
+// this keyword
+
+// let person={
+//     firstName: 'vaibhav',
+//     lastName: 'Nirmal',
+//     sayHello (){
+//         console.log("My name is "+ this.firstName+ " "+ this.lastName);
+//     }
+// };
+
+// person.sayHello();
+
+
+
+
+
+// console.log(Math.PI+"\n"+ Math.sqrt(9)
+// +"\n"+ Math.pow(5,2));
+
+// console.log(Math.min(4,3,2,6,7,8,9,0));
+// console.log(Math.max(4,3,2,6,7,8,9,0));
+
+
+//random number
+// console.log(Math.floor(Math.random()*10));
+
+
+
+
+// let x= new Date();
+// console.log(x.getDate()+"-"+(x.getMonth()+1)+"-"+x.getFullYear());
+
+// // console.log(x.getHours()-12);
+// console.log(x.getDay());
+
+
+
+
+// let productData={
+//     productName: "mobile",
+//     productBrand: "OnePlus",
+//     model: "Nord2T",
+//     price: 30000
+// }
+
+// console.log(productData.productBrand.toUpperCase()+ " "+ productData.model+" : "+ productData.price);
+
+
+
+
+
+
+// getter setter
+
+// let userDetails={
+//     userName: "Vaibhav Nirmal",
+//     userMail: "vaibhavnirmal2001@gmail.com",
+//     age: 21,
+//     Degree: "B. Tech",
+//     get getName(){
+//         return this.userName.toUpperCase();
+//     },
+    
+//     set setName( name){
+//         this.userName=name.toUpperCase();
+//     }
+// }
+// // console.log(userDetails)
+// userDetails.setName="Pratibha Nirmal";
+// console.log(userDetails.getName);
+
+
+
+
+
+// obj contructor
+
+// let student1={
+//     firstName: "Vaibhav",
+//     lastName: "Nirmal",
+//     age: 21,
+// }
+// function student(firstName,lastName,age){
+//     this.firstName=firstName;
+//     this.lastName=lastName;
+//     this.age=age;
+// }
+
+// let st1= new student("Pratibha","Nirmal",22);
+// st1.nationality="Indian";
+// console.log(student1);
+// console.log(st1);
+
+
+
+
+//hoisting : to left or pull
+// i.e. we can call function before declairation or after declairation
+// hello();
+
+// function hello(){
+//     console.log("hello world")
+// }
+
+// let x;
+// x=7;
+// console.log(x);
+
+
+
+
+
+// DOM
+
+
+
+
+
 
 
 
